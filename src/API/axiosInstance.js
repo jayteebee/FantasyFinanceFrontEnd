efef4fee1855067931367ab8e001857a20875d98
Jethro_Block <jethroblock22@gmail.COM>
2023-06-22 14:17:54 +0100

    update axios instances with deployed url

--- src/API/axiosInstance.js
+++ src/API/axiosInstance.js
@@ -1,9 +1,9 @@
-// Configuring the Axios Instance for the API
-
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: process.env.NODE_ENV === 'production' 
+                ? 'https://fantasy-finance-c6a108a05dfa.herokuapp.com/' 
+                : 'http://localhost:4000',
     headers: {
         'Content-Type': 'application/json'
     },
--- src/API/axiosInstanceWithToken.js
+++ src/API/axiosInstanceWithToken.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 const token = window.localStorage.getItem("token");
 
 const axiosInstanceWithToken = axios.create({
-    baseURL: "http://localhost:4000",
+    baseURL: process.env.NODE_ENV === 'production' 
+                ? 'https://fantasy-finance-c6a108a05dfa.herokuapp.com/' 
+                : 'http://localhost:4000',
     headers: {
         'Content-Type': 'application/json',
         'Authorization': token,
